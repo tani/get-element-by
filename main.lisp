1e1d7769ea61d5aa1b566e45c400a9a67fa706fc
TANIGUCHI Masaya <mew@cat.ovh>
2015-06-28 00:01:21 +0900

    add asd file and source code

---
+++ main.lisp
@@ -0,0 +1,37 @@
+(defpackage get-element-by
+  (:use common-lisp)
+  (:nickname cl-html)
+  (:export get-elements-by
+	   get-element-by))
+
+(in-package get-element-by :use 'common-lisp)
+
+(defun get-elements-by (tag-name-or-attribute value body)
+  "
+  DESCRIBE:
+    getElementById like interface for cl-html-parser
+  USAGE:
+    (defvar tree (net.html.parser:parse-html string-of-html))
+    (get-elements-by :tag-name :h1 tree)
+    (get-elements-by :id \"id0\" tree)
+    (get-elements-by :class \"class1\" tree)
+    (setf (getf (nth 2 (get-elements-by :class \"class1\" tree)) :class) \"class2\")
+  RETURN-TYPE:
+    LIST
+  "
+  (let (elements target test)
+    (if (eq tag-name-or-attribute :tag-name)
+	(setf target value
+	      test (lambda (x) (eq (car x) value)))
+	(setf target tag-name-or-attribute
+	      test (lambda (x) (string= (getf (cdr x) target) value))))
+    (labels ((get-elements-by-rec (body)
+	       (unless (or (not body) (atom body))
+		 (when (member target body) (push body elements))
+		 (mapcar #'get-elements-by-rec body))))
+      (get-elements-by-rec body))
+    (nreverse (delete-if-not test elements))))
+
+(defun get-element-by (tag-name-or-attribute value body)
+  "Alias for `(car (get-elements-by ...))'"
+  (car (get-elements-by tag-name-or-attribute value body)))
